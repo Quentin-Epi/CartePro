e882b644d6d55e87d2e51d7f74c5a9273d76890e
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-09-01 16:57:54 +0200

    feat: add init.sql

---
+++ init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    mail VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL,
+    password_hash TEXT NOT NULL,
+    role VARCHAR(32) NOT NULL,
+    state VARCHAR(32) NOT NULL,
+    created_at BIGINT NOT NULL
+);
