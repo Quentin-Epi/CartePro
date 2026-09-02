8b25fb74b23243fbe8d3ce0844b50950f8d0c9c6
tom <tom.chatelain@epitech.eu>
2026-09-02 09:17:49 +0200

    feat: user balance and make watermark less instrusive

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,7 @@ import Login from "./pages/login"
 import Signup from "./pages/signup"
 import PartnerPaymentPage from "./pages/PartnerPages/payment"
 import EmployeePaymentPage from "./pages/EmployeePages/payment"
+import EmployeeBalancePage from "./pages/EmployeePages/ballance"
 import { Header } from "./components/Header"
 
 export default function App() {
@@ -12,8 +13,8 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/EmployeePages/payment" element={<><Header /><EmployeePaymentPage /></>} />
+        <Route path="/EmployeePages/balance" element={<><Header /><EmployeeBalancePage /></>} />
         <Route path="/PartnerPages/payment" element={<><Header /><PartnerPaymentPage /></>} />
-        {/* Redirection par défaut vers le login */}
         <Route path="*" element={<><Navigate to="/EmployeePages/payment" replace /></>} />
       </Routes>
     </BrowserRouter>
