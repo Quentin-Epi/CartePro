454ce9f9c928267815fe1593066ec148aaa3099e
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-09-04 10:12:13 +0200

    feat: update all SQL tables

--- init.sql
+++ init.sql
@@ -1,42 +1,45 @@
 CREATE TABLE users (
-    id UUID PRIMARY KEY,
-    mail VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
-    password TEXT NOT NULL,
-    role VARCHAR(32) NOT NULL,
-    created_at BIGINT NOT NULL
+  id UUID PRIMARY KEY,
+  mail VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255) NOT NULL,
+  password TEXT NOT NULL,
+  role VARCHAR(32) NOT NULL,
+  created_at BIGINT NOT NULL
 );
- 
+
 CREATE TABLE state (
-    id UUID PRIMARY KEY REFERENCES users(id),
-    state VARCHAR(32) NOT NULL,
-    reason VARCHAR(255),
-    modified_at DATE NOT NULL
+  id UUID PRIMARY KEY REFERENCES users(id),
+  state VARCHAR(32) NOT NULL,
+  reason VARCHAR(255),
+  modified_at BIGINT NOT NULL
 );
- 
+
 CREATE TABLE employee (
-    id UUID PRIMARY KEY REFERENCES users(id),
-    balance INT2
+  id UUID PRIMARY KEY REFERENCES users(id),
+  balance REAL,
+  qr_token VARCHAR(255),
+  qr_token_created_at BIGINT
 );
- 
+
 CREATE TABLE partner (
-    id UUID PRIMARY KEY REFERENCES users(id),
-    siren INT4 UNIQUE,
-    social_obj VARCHAR(255),
-    highlight BOOL,
-    verification BOOL,
-    category VARCHAR(255)
+  id UUID PRIMARY KEY REFERENCES users(id),
+  coordinate POINT,
+  siren INT4 UNIQUE,
+  social_obj VARCHAR(255),
+  highlight BOOL,
+  verification BOOL,
+  category VARCHAR(255)
 );
- 
+
 CREATE TABLE admin (
-    id UUID PRIMARY KEY REFERENCES users(id),
+  id UUID PRIMARY KEY REFERENCES users(id)
 );
- 
+
 CREATE TABLE transaction (
-    id UUID PRIMARY,
-    do_at DATE NOT NULL,
-    success BOOL,
-    value INT2,
-    partner_id UUID KEY REFERENCES users(partner_id),
-    employee_id UUID KEY REFERENCES users(employee_id),
-);
+  id UUID PRIMARY KEY,
+  timestamp BIGINT NOT NULL,
+  success BOOL,
+  value REAL,
+  partner_id UUID REFERENCES partner(id),
+  employee_id UUID REFERENCES employee(id),
+) 
